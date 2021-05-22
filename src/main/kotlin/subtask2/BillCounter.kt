e2c2f345c0dbe10a1996d282cddd13b7c74d0db7
HavWho <workplacer2019@gmail.com>
2021-05-23 00:31:13 +0300

    add: task2 has been completed

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,10 +1,17 @@
 package subtask2
 
+import kotlin.math.abs
+
 class BillCounter {
 
-    // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val billHalf = bill.sum().minus(bill[k]).div(2)
+
+        if (billHalf.equals(b))
+            return "Bon Appetit"
+        else{
+            return abs(billHalf - b).toString()
+        }
     }
 }
